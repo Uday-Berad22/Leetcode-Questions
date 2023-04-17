2c70744e4a14a9182ebd539205a070c87d04ba14
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-04-17 19:16:51 +0530

    Added solution - LeetHub

--- Largest prime factor - GFG/largest-prime-factor.cpp
+++ Largest prime factor - GFG/largest-prime-factor.cpp
@@ -6,25 +6,21 @@ using namespace std;
 
 const int Ni=1e5+10;
 vector<bool> is_prime(Ni,true);
-vector<int> highest_prime(Ni,0);
-vector<int> lowest_prime(Ni,0);
 class Solution{
 public: 
     long long int largestPrimeFactor(int n){
-        is_prime[0]=is_prime[1]=false;
-     for(int i=2;i<Ni;i++){
+        long long hp=0;
+     for(int i=2;i<=n;i++){
         if(is_prime[i]==true){
-            highest_prime[i]=lowest_prime[i]=i;
-            for(int j=2*i;j<Ni;j+=i){
+            if(i==n)
+            return n;
+            for(int j=2*i;j<=n;j+=i){
                 is_prime[j]=false;
-                if(lowest_prime[j]==0){
-                    lowest_prime[j]=i;
-                }
-                highest_prime[j]=i;
+                if(j==n)hp=i;
             }
         }
     }
-        return highest_prime[n];
+        return hp;
     }
 };
 
