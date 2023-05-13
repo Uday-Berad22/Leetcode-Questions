9e207cba8921b27460cbe4f9a7827cb2b7154217
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-05-13 11:11:16 +0530

    Added solution - LeetHub

--- Bit Magic - GFG/bit-magic.cpp
+++ Bit Magic - GFG/bit-magic.cpp
@@ -32,12 +32,6 @@ class Solution {
   public:
     int bitMagic(int n, vector<int> &arr) {
         // code 
-        int count=0;
-        for(int i=0;i<n/2;i++){
-            if(arr[i]==arr[n-i-1])
-            count++;
-        }
-        if(count==n) return 0;
         int c1=0,c0=0;
         for(int i=0;i<n/2;i++){
             if(arr[i]==1&&arr[n-i-1]==0){
@@ -48,25 +42,10 @@ class Solution {
                 c0++;
             }
         }
-        int ans=0;
-        int x=c1;
-        // if(1c%2==0){
-            ans=c0+(c1/2)+(c1%2);
-        // }
-        c1=0;c0=0;
-        int a2=0;
-        for(int i=0;i<n/2;i++){
-            if(arr[i]==1&&arr[n-i-1]==0){
-                c0++;
-            }
-            
-            else if(arr[i]==0&&arr[n-i-1]==1){
-                c1++;
-            }
-        }
-        a2=c0+(c1/2)+(c1%2);
-        x=x+c1;
-        
+        if(c1==0&&c0==0) return 0;
+        int ans=c0+(c1/2)+(c1%2);
+        int x=c1+c0;
+        int a2=c1+(c0/2)+(c0%2);
         return min(min(a2,ans),((x/2)+(x%2)));
     }
 };
