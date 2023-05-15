057b01b8f325d823536fe92bcb345def60e5ac6f
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-05-15 20:01:04 +0530

    Added solution - LeetHub

--- Count Total Setbits - GFG/count-total-setbits.cpp
+++ Count Total Setbits - GFG/count-total-setbits.cpp
@@ -1,68 +1,49 @@
 //{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
-// User function Template for C++
 
-class Solution{
-public:
-    int countBits(int n){
-        int x=log2(n)+1;
-         unordered_map<int,int> m;
-         if(n==1)
-         return 1;
-         if(n==2)
-         return 2;
-         if(n==3)
-         return 4;
-         m[1]=1;
-         m[3]=4;
-         for(int i=3;i<x;i++){
-             int y=1<<i;
-             int size=1<<(i-1);
-             int k=1<<(i-1);
-            m[y-1]=((size*(i-1))/2)+size+m[k-1];
-         }
-         int fans=0;
-         int  z=1<<(((int)log2(n)));
-         int stop=(int)log2(n);
-         int diff=n-z+1;
-         int bro=0;
-         for(int i=0;i<stop;i++){
-             int ans=diff/pow(2,i);
-             ans=ans/2;
-             ans=ans*pow(2,i);
-             
-             int flag=0;
-             if(1&(n>>i)){
-                 int b=pow(2,i);
-                 flag=diff%b;
-             }
-             ans=ans+flag;
-             fans+=ans;
-             bro=1;;
-         } 
-         if(bro==1)
-         fans+=diff;
-        return fans+m[z-1];
+// } Driver Code Ends
+
+class Solution {
+  public:
+    long long countBits(long long N) {
+        long long ans=0;
+        int n=log2(N)+1;
+        N=N+1;
+        int j=0;
+        for(int i=0;i<n;i++){
+            int x=pow(2,j);
+            long long temp=0;
+            temp=(((N)/x)/2)*x;
+            if((N%x)!=0){
+                if((N/x)%2!=0)
+                temp+=(N%x);
+            }
+            ans+=temp;
+            j++;
+        }
+        return ans;
     }
 };
+
 
 //{ Driver Code Starts.
 
 int main(){
     int t;
-    cin>>t;
+    scanf("%d ",&t);
     while(t--){
-        int N;
-        cin>>N;
         
-        Solution ob;
-        cout<<ob.countBits(N)<<"\n";
+        long long N;
+        scanf("%lld",&N);
+        
+        Solution obj;
+        long long res = obj.countBits(N);
+        
+        cout<<res<<endl;
+        
     }
-    return 0;
 }
+
 // } Driver Code Ends
