4c761aed0924759e09d465bae8e67afa4251db35
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-06-13 08:28:04 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -8,31 +8,24 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
+// User function Template for C++
+
 class Solution{
   public:
-    int help_function(int index,int n,int price[],vector<vector<int>> &dp){
-        if(index>n){
-            return 0;
-        } 
-        int a=0;
-        if(index!=n&&dp[index+1][n]==-1){
-            dp[index+1][n]=help_function(index+1,n,price,dp);
-        }
-        if(index!=n){
-            a=dp[index+1][n];
-        }
-         if(dp[index][n-index]==-1){
-           dp[index][n-index]=max(help_function(index,n-index,price,dp),0);
-        }
-        int b=dp[index][n-index]+price[index-1];
-        dp[index][n]=max(a,b);
-        return max(max(a,b),0);
-    }
     int cutRod(int price[], int n) {
-        vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
-       int ans=help_function(1,n,price,dp);
-        return ans;
-        
+        vector<int> dp(n+1,0);
+        int temp=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i>j){
+                    continue;
+                }
+                else{
+                    dp[j]=max(price[i-1]+dp[j-i],dp[j]);
+                }
+            }
+        }
+        return dp[n];
     }
 };
 
