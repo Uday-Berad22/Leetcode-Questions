be258eef6bf46c66ccdc70e382015e71c998f6e3
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-07-02 14:27:44 +0530

    Added solution - LeetHub

--- Bit Difference - GFG/bit-difference.cpp
+++ Bit Difference - GFG/bit-difference.cpp
@@ -1,45 +1,63 @@
 //{ Driver Code Starts
-//Initial Template for C++
+// Initial Template for C++
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
 // } Driver Code Ends
-//User function Template for C++
-
+// User function Template for C++
+const int M=1e9+7;
 class Solution{
-    public:
-    // Function to find number of bits needed to be flipped to convert A to B
-    int countBitsFlip(int a, int b){
-        
-        // Your logic here
-        int c=a^b;
-        int count=0;
-        for(int i=31;i>=0;i--)
-        {
-            if(((c>>i)&1)!=0)
-            count++;
+public:
+    int countBits(int N, long long int A[])
+    {
+        // code here
+        vector<long long > a(64,0);
+        for(int i=0;i<N;i++){
+            for(int j=0;j<64;j++){
+                if((A[i]>>j)&1){
+                    a[j]=(a[j]+1)%M;
+                }
+            }
         }
-        return count;
+        long long  count=0;
+         for(int i=0;i<N;i++){
+            for(int j=0;j<64;j++){
+                if(((A[i]>>j)&1)==0&&a[j]!=0){
+                    count=((count+a[j]))%M;
+                   
+                }
+            }
+        }
+         for(int i=0;i<N;i++){
+            for(int j=0;j<64;j++){
+                if(((A[i]>>j)&1)==0&&a[j]!=0){
+                    count=((count+a[j]))%M;
+                   
+                }
+            }
+        }
+        // cout<<M<<endl;
+        return (int)count;
+
     }
 };
 
 //{ Driver Code Starts.
 
-// Driver Code
-int main()
-{
-	int t;
-	cin>>t;// input the testcases
-	while(t--) //while testcases exist
-	{
-		int a,b;
-		cin>>a>>b; //input a and b
-
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        long long int A[N];
+        for(int i = 0;i < N; i++)
+            cin>>A[i];
+        
         Solution ob;
-		cout<<ob.countBitsFlip(a, b)<<endl;
-	}
-	return 0;
+        cout<<ob.countBits(N, A)<<"\n";
+    }
+    return 0;
 }
 // } Driver Code Ends
