385a30c93060bb13c7b37cf9131dabe2a5785387
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-07-24 16:49:16 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -5,31 +5,45 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    // Function to detect cycle in an undirected graph.
-    bool detect_cycle_in_Graph(vector<int> G[],vector<bool> &visited,int node,int parent){
-    visited[node]=true;
-    bool ans=false;
-    for(auto nbr: G[node]){
-        if(visited[nbr]==true&&nbr!=parent){
-            return true;
-        }
-        else if(visited[nbr]==false){
-            ans=ans||detect_cycle_in_Graph(G,visited,nbr,node);
+   bool detect_cycle_in_Graph_using_BFS(vector<int> G[],int V)
+{
+    int n = V;
+    vector<bool> visited(n, false);
+    queue<pair<int,int>> q;
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i])
+        {
+            q.push({i,i});
+            visited[i] = true;
+            while (!q.empty())
+            {
+                    int node = q.front().first;
+                    int parent=q.front().second;
+                    q.pop();
+                    for (auto nbr : G[node])
+                    {                             
+                        if (!visited[nbr])
+                        {
+                            q.push({nbr,node});
+                            visited[nbr] = true;
+                        }
+                        else if (visited[nbr] == true && parent != nbr)
+                        {
+                            return true;
+                        }
+                    }
+            }
         }
     }
-    return ans;
-    }
-    bool isCycle(int V, vector<int> adj[]) {
-        // Code here
-           vector<bool> visited(V,false);
-           for(int i=0;i<V;i++){
-               if(!visited[i]){
-                 if(detect_cycle_in_Graph(adj,visited,i,i)){
-                     return true;
-                }
-               }
-           }
-            return false;
+    return false;
+}
+    bool isCycle(int V, vector<int> G[]) {
+        if (detect_cycle_in_Graph_using_BFS(G,V))
+        {
+             return true;
+        }
+         return false;
     }
 };
 
