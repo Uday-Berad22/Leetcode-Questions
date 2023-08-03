d3dc2daa35e1c97dbf7665f7fc2079e30d1ee3c9
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-08-03 19:00:18 +0530

    Added solution - LeetHub

--- Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
@@ -6,48 +6,45 @@ using namespace std;
 
 // } Driver Code Ends
 // User function Template for C++
+
+
+// User function Template for C++
 class Solution {
   public:
      vector<int> shortestPath(int N,int M, vector<vector<int>>& edges){
-         vector<pair<int,int>>adj[N];
-            for(int i=0;i<M;++i)
-            {
-                int u=edges[i][0];
-                int v=edges[i][1];
-                int z=edges[i][2];
-                adj[u].push_back({v,z});
+        // code here
+        vector<vector<pair<int,int>>> G(N);
+        vector<int> ans(N,INT_MAX);
+        ans[0]=0;
+        for(auto a: edges){
+
+                G[a[0]].push_back({a[1],a[2]});
+
+        }
+        priority_queue<pair<int,int>> pq;
+        vector<bool> visited(N,false);
+        pq.push({0,0});
+        visited[0]=true;
+        while(!pq.empty()){
+            int node=pq.top().second;
+            int val=pq.top().first;
+            ans[node]=min(-1*val,ans[node]);
+            pq.pop();
+            // if(visited[node]==true){
+            //     continue;
+            // }
+            for(auto nbr: G[node]){
+                // if(visited[nbr.first]==false){
+                if(-1*(val+(-1)*nbr.second)<ans[nbr.first])
+                pq.push({val+(-1)*nbr.second,nbr.first});
+                visited[nbr.first]=true;
+                // }
             }
-            vector<int>dist(N);
-       for(int i=0;i<N;++i)
-       dist[i]=INT_MAX;
-       dist[0]=0;
-       priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-       //{weight,node}
-       pq.push({0,0});
-       while(!pq.empty())
-       {
-           int node=pq.top().second;
-           int weight=pq.top().first;
-           pq.pop();
-           for(auto it:adj[node])
-           {
-               int adjNode=it.first;
-               int weightage=it.second;
-               if(weight+weightage<dist[adjNode])
-               {
-                  dist[adjNode]= weight+weightage;
-                  pq.push({dist[adjNode],adjNode});
-               }
-           }
-       }
-      for(int i=0;i<dist.size();++i)
-      {
-          if(dist[i]==INT_MAX)
-          dist[i]=-1;
-      }
-       
-      return dist; 
-      
+        }
+        for(int i=0;i<N;i++){
+            if(ans[i]==INT_MAX) ans[i]=-1;
+        }
+        return ans;
     }
 };
 
