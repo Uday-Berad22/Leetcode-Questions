5a4fd06a272be4b0760556cfc090263d5c4930eb
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-08-19 09:47:56 +0530

    Added solution - LeetHub

--- Subarray with given sum - GFG/subarray-with-given-sum.cpp
+++ Subarray with given sum - GFG/subarray-with-given-sum.cpp
@@ -8,39 +8,35 @@ class Solution
 {
     public:
     //Function to find a continuous sub-array which adds up to a given number.
-    vector<int> subarraySum(vector<int>a, int n, long long s)
+    vector<int> subarraySum(vector<int>arr, int n, long long k)
     {
-        // Your code 
-        // long long sum=*accumulate(arr.begin(),arr.end());
-        long long sum=0;
-        for(int i=0;i<a.size();i++){
-            sum+=a[i];
+        if(k==0)return {-1};
+       long long sum=0;
+       int i=0,j=0;
+       int count=0;
+       while(i<n||j<=i){
+           if(sum==k){
+               return {j+1,i};
+           }
+           else if(sum>k){
+               sum-=arr[j];
+               j++;
+           }
+           else if(i<n){
+               sum+=arr[i];
+              i++;
+           }
+           else{
+               break;
+           }
+        //   cout<<i<<" "<<j<<" "<<sum<<endl;
+        //   count++;
+        //   if(count>=1e5+10) return {1,2};
+       }
+        if(sum==k){
+            return {j+1,i};
         }
-        int i=0,j=a.size()-1;
-        while(i<n&&i>=0&&j<n&&j>=0){
-            // cout<<sum<<" "<<i<<endl;
-            if(sum==s){
-                return {(i+1),(j+1)};
-            }
-            if(sum>s){
-                if(sum-a[j]>=s){
-                    // cout<<sum-a[j]<<"**"<<a[j]<<" "<<j<<endl;
-                sum=sum-a[j];
-                j--;
-                }
-                else{
-                    sum-=a[i];
-                    i++;
-                }
-                
-            }
-            else if(sum<s){
-                j++;
-                sum+=a[j];
-            }
-        }
-      
-        return {-1};
+       return {-1};
     }
 };
 
