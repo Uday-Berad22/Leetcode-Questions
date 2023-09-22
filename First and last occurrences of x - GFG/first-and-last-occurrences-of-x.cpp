943e5303c5d0178beb82bea598423383c1e301e6
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-09-22 09:25:23 +0530

    Added solution - LeetHub

--- First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
@@ -20,13 +20,27 @@ class Solution
         if(high==n) return -1;
         return high;
     }
+    int up(int arr[],int n,int x){
+        int low=-1;
+        int high=n;
+        while(high-low>1){
+            int mid=(high+low)>>1;
+            if(arr[mid]<=x){
+                low=mid;
+            }
+            else
+            high=mid;
+        }
+        if(low==-1) return -1;
+        return low;
+    }
     vector<int> find(int arr[], int n , int x )
     {
         // code here
         if(binary_search(arr,arr+n,x)){
             int i=lower(arr,n,x);
-            int j=upper_bound(arr,arr+n,x)-arr;
-            return {i,j-1};
+            int j=up(arr,n,x);
+            return {i,j};
         }
         return {-1,-1};
     }
