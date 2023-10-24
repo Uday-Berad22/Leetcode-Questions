5f7f947a3579733e8e0b3731d8bdd810246d200a
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-10-24 23:19:56 +0530

    Added solution - LeetHub

--- Replace O's with X's - GFG/replace-os-with-xs.cpp
+++ Replace O's with X's - GFG/replace-os-with-xs.cpp
@@ -10,68 +10,41 @@ using namespace std;
 class Solution{
     vector<vector<int>> directions={{1,0},{0,1},{-1,0},{0,-1}};
 public:
-   void dfs1(vector<vector<bool> > & visited,int i,int j,int &N,int &M,vector<vector<char>>& matrix){
-       if((i==N||j==M)||(i<0||j<0)){
-           return;
-       }
-       if(visited[i][j]==true||matrix[i][j]=='X'){
-           return ;
-       }
-       visited[i][j]=true;
-       dfs1(visited,i+1,j,N,M,matrix);
-       dfs1(visited,i-1,j,N,M,matrix);
-       dfs1(visited,i,j+1,N,M,matrix);
-       dfs1(visited,i,j-1,N,M,matrix);
+    void DFS(int x,int y,vector<vector<char>> &mat,vector<vector<bool>> &visited,bool &boundary){
+        int n=mat.size();
+        int m=mat[0].size();
+        visited[x][y]=true;
+        if(boundary==false){
+            mat[x][y]='X';
+        }
+        for(auto &dir: directions)
+        {
+            int xx=x+dir[0];
+            int yy=y+dir[1];
+            if(xx<n&&yy<m&&xx>=0&&yy>=0&&visited[xx][yy]==false&&mat[xx][yy]=='O')
+                DFS(xx,yy,mat,visited,boundary);
+        }       
     }
-   void dfs(vector<vector<bool> > & visited,int i,int j,int &N,int &M,vector<vector<char>>& matrix){
-       if((i==N||j==M)||(i<0||j<0)){
-           return;
-       }
-       if(visited[i][j]==true||matrix[i][j]=='X'){
-           return ;
-       }
-       visited[i][j]=true;
-       dfs(visited,i+1,j,N,M,matrix);
-       dfs(visited,i-1,j,N,M,matrix);
-       dfs(visited,i,j+1,N,M,matrix);
-       dfs(visited,i,j-1,N,M,matrix);
-       matrix[i][j]='X';
-    }
-
-    vector<vector<char>> fill(int N, int M, vector<vector<char>> &matrix)
+    vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
     {
-        // code here
-       vector<vector<bool>> visited(N,vector<bool> (M,false));
-        for(int i=0;i<M;i++){
-            if(matrix[0][i]=='O'&&visited[0][i]==false){
-                 dfs1(visited,0,i,N,M,matrix);
-            }
-        }
-        for(int i=1;i<N;i++){
-            if(matrix[i][0]=='O'&&visited[i][0]==false){
-                 dfs1(visited,i,0,N,M,matrix);
-            }
-        }
-        for(int i=0;i<M;i++){
-            if(matrix[N-1][i]=='O'&&visited[N-1][i]==false){
-              dfs1(visited,N-1,i,N,M,matrix);
-            }
-        }
-        for(int i=1;i<N;i++){
-            if(matrix[i][M-1]=='O'&&visited[i][M-1]==false){
-                 dfs1(visited,i,M-1,N,M,matrix);
-            }
-        }
-        int count=0;
-        for(int i=1;i<N-1;i++){
-            for(int j=1;j<M-1;j++){
-                if(matrix[i][j]=='O'&&visited[i][j]==false){
-                   dfs(visited,i,j,N,M,matrix);
-                   count++;
+        vector<vector<bool>> visited(n,vector<bool>(m,false));
+        bool boundary=true;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0||i==n-1||j==0||j==m-1){
+                    if(visited[i][j]==false&&mat[i][j]=='O')
+                    DFS(i,j,mat,visited,boundary);
                 }
             }
         }
-        return matrix;
+        boundary=false;
+        for(int i=1;i<n-1;i++){
+            for(int j=1;j<m-1;j++){
+                    if(visited[i][j]==false&&mat[i][j]=='O')
+                    DFS(i,j,mat,visited,boundary);
+            }
+        }
+        return mat;
     }
 };
 
