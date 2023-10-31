0765ca40120fa759b26a4fb691caae7fa438fe43
Uday-Berad22 <120621657+Uday-Berad22@users.noreply.github.com>
2023-10-31 14:38:09 +0530

    Added solution - LeetHub

--- Move all zeroes to end of array - GFG/move-all-zeroes-to-end-of-array.cpp
+++ Move all zeroes to end of array - GFG/move-all-zeroes-to-end-of-array.cpp
@@ -9,16 +9,9 @@ public:
 	void pushZerosToEnd(int a[], int n) {
 	    // code here
 	    int j=0;
-	    int i=0;
-	    while(j<n){
-	        if(i<n){
-	            if(a[i]!=0){
-	            a[j]=a[i];
-	            j++;}
-                i++;
-	        }
-	        else{
-	            a[j]=0;
+	    for(int i=0;i<n;i++){
+	        if(a[i]!=0){
+	            swap(a[i],a[j]);
 	            j++;
 	        }
 	    }
